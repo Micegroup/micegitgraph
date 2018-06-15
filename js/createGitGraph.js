c6e5892e72a0e12f00dcfcb5c6602250115cf8cb
dario.telese <dario.telese@micegroup.it>
2018-06-15 16:00:08 +0200

    possibility to insert myFunction in parameters

--- js/createGitGraph.js
+++ js/createGitGraph.js
@@ -1,4 +1,3 @@
-
 //IMPORTANT
 //check always Api version
 
@@ -6,23 +5,34 @@ var gitgraph;
 var branches = new Array(); //save branches 
 var nameProject;
 var urlBase;
-var login; // variable to create url with or without token
+var login; // variable for to create url with or without token
 var headers;//for ajax call
+var _function; //use for new function insert by user
 
 
 //function to create gitgraph base and to call Api for save token if there are username and password
-function createGitgraph(url, version, username, password) {
+// * url =  base url of project gitLab - REQUIRED
+// * version = version of gitLab's project (v3 or v4) - REQUIRED
+// * username =  gitLab's username (REQUIRED for private project)
+// * password = gitLab's password (REQUIRED for private project)
+// * newFunction = (NOT REQUIRED) It will be called on the "node" click
+function createGitGraph(url, version, username, password, newFunction) {
 
-	console.log(url)
-
+	if (newFunction == null) {
+		_function = function () {
+			console.log("Oh, you clicked my commit?!\nVisit: https://github.com/Micegroup/micegitgraph");
+		}
+	}
+	else {
+		_function = newFunction
+	}
 
 	// splite url for api
-	var n = url.indexOf("/",9)
-	urlBase = url.substring(0,n);
-	nameProject = url.substring(n+1,url.lenght)
-	nameProject = nameProject.replace(/\//g,"%2F");
-	nameProject = nameProject.replace(".git",'')
-
+	var n = url.indexOf("/", 9)
+	urlBase = url.substring(0, n);
+	nameProject = url.substring(n + 1, url.lenght)
+	nameProject = nameProject.replace(/\//g, "%2F");
+	nameProject = nameProject.replace(".git", '')
 	urlApi = urlBase + "/api/" + version;
 
 	console.log("INIT CREATE GITGRAPH")
@@ -55,7 +65,7 @@ function createGitgraph(url, version, username, password) {
 				displayAuthor: false,
 				displayBranch: false,
 				displayHash: false,
-			}
+			},
 		},
 		arrow: {
 			size: 8,
@@ -103,49 +113,38 @@ function createGitgraph(url, version, username, password) {
 
 			}
 		});
-
 	}
 	else {
 		login = false;
 		headers = null
 		main();
 	}
-
 } // END function createGitgraph
 
 
 //function that calls API gitLab and create dynamically node of graph
 function main() {
-
 	//creazione degli url che verranno poi utilizzati nelle chiamate ajax API
 	//url for api calls
 	if (login == true) {
 		var urlGetBranches = urlApi + "/projects/" + nameProject + "/repository/branches";
-		console.log("urlGetBranches: "+urlGetBranches)
 		//per_page	Number of items to list for page (default: 20, max: 100)
 		//the request is filtered by "merged", in order to return only the merge requests in that state
 		var urlGetMergeRequest = urlApi + "/projects/" + nameProject + "/merge_requests?state=merged&per_page=100";
-		console.log("urlGetMergeRequest: "+urlGetMergeRequest)
 	}
 	else {
 		var urlGetBranches = urlApi + "/projects/" + nameProject + "/repository/branches";
-		console.log("urlGetBranches: "+urlGetBranches)
 		//per_page		Number of items to list for page (default: 20, max: 100)
 		var urlGetMergeRequest = urlApi + "/projects/" + nameProject + "/merge_requests?state=merged&per_page=100";
-		console.log("urlGetMergeRequest: "+urlGetMergeRequest)
 	}
-
-
 
 	branches.push(gitgraph.branch("master")); //branche master in branches[0]
 	branches.find(x => x.name == "master").commit({
 		author: "dario.telese@micegroup.it",
 		column: 0,          // index of row to show this branches 
-		message : "init Master",
+		message: "init Master",
 		onClick: function (commit) {
-			alert("Title: init Master\n" +
-				"Author : " + commit.author + "\n" +
-				"Date: " + commit.date);
+			console.log("Init branche Master", commit);
 		}
 	});
 
@@ -163,14 +162,10 @@ function main() {
 					branches.find(x => x.name == element.name).commit({  //first commit of branche
 						message: "init " + element.name,
 						sha: element.id,
+						author: element.commit.author_email,
 						onClick: function (commit) {
-							console.log("Oh, you clicked my commit?!", commit);
-							alert("Title: init " + element.name + "\n" +
-								"Message: " + element.commit.message + "\n" +
-								"Author : " + element.commit.author_email + "\n" +
-								"Date: " + element.commit.committed_date);
+							console.log("Init branche " + element.name, commit);
 						}
-
 					})
 				}
 			});
@@ -180,7 +175,6 @@ function main() {
 				url: urlGetMergeRequest,
 				headers: headers,
 				success: function (data) {
-
 					data = data.reverse(); //to create the graph in order from left to right (from the oldest to the new)
 					data.forEach(element => {
 						var source = branches.find(x => x.name == element.source_branch);
@@ -188,13 +182,7 @@ function main() {
 						if (source != undefined && target != undefined) { //if branche exists, it's possible to do merge
 							source.merge(target, {
 								author: element.author.username,
-								onClick: function (commit) {
-									console.log("Oh, you clicked my commit?!", commit);
-									alert("Title: " + element.title + "\n" +
-										"Message: " + commit.message + "\n" +
-										"Author : " + commit.author + "\n" +
-										"Date: " + commit.date);
-								}
+								onClick: _function
 							})
 						}
 					}) //end forEach
